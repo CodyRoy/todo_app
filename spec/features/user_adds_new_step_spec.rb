72c96dcf33c72af57279cc6dc711d614d055cab8
Cody Roy <cr554@localhost.localdomain>
2017-06-13 10:16:34 -0400

    added the before/after clauses to the user_adds_new_step_spec.rb tests

--- spec/features/user_adds_new_step_spec.rb
+++ spec/features/user_adds_new_step_spec.rb
@@ -1,15 +1,21 @@
 require "rails_helper"
 
 feature "user creates a new step" do
+	before(:each) do	
+		@list = List.create(title: "list1", desc: "dummylist")
+		@task = Task.create(content: "dummytask", list_id:1)
+	end
 	scenario "successfully" do
-		list = List.create(title: "list1", desc: "dummylist")
-		task = Task.create(content: "dummytask", list_id:1)
-		visit list_task_path(list, task) 
+		visit list_task_path(@list, @task) 
 		click_on "New Step"
 		fill_in "step_content", with: "next step"
 		fill_in "step_task_id", with: 1
 		click_on "Create Step"
 
 		expect(page).to have_css "p", text: "next step"
+	end
+	after(:each) do
+		@list.destroy
+		@task.destroy
 	end
 end
